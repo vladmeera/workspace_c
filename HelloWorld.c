d2cd6dc73ac41e312130f8398d9216f39e3a3cde
vladmeera <mirgorodvld@gmail.com>
2025-03-15 19:14:04 -0400

    Add comments

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+// This is a comment
+/*
+    Multi 
+    line 
+    comment
+*/
 int main() {
+
+    /*
+        escape sequence = character combination consisting of a backslash (\) 
+        followed by one or more characters or combination of digits
+
+                They specify actions within a line or string of text
+                \n - new line
+                \t - tab
+                \b - backspace
+    */
 
     printf("Hello World!\n");
     printf("I love my family!!");
