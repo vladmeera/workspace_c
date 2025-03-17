e07383a90c0e6c68c5f45986f562aad9dac69b1f
vladmeera <mirgorodvld@gmail.com>
2025-03-17 10:24:38 -0400

    For Loop

---
+++ ForLoops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // for loop - repeats a section of code a limited amount of time
+
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("\n%d", i);
+    }
+
+
+    return 0;
+}
