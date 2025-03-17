f4ff430ae58d7bf47d8133c430ad6ab4465d208d
vladmeera <mirgorodvld@gmail.com>
2025-03-17 01:54:48 -0400

    Added math functions

---
+++ MathFunc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+
+    double A = sqrt(9);
+    double B = pow(5, 2);
+    int C = round(5.4);
+    int D = ceil(5.1);
+    int E = floor(5.9);
+    double F = fabs(-53);
+    double G = log(5);
+    double H = sin(90);
+    double I = cos(30);
+    double J = tan(45);
+
+    printf("\n%lf", I);
+    printf("\n%d", E);
+
+    return 0;
+}
