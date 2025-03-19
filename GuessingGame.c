a5cd97fb9aabb7995d2fcf2294a038aa3b9a68da
vladmeera <mirgorodvld@gmail.com>
2025-03-18 20:30:55 -0400

    Update

---
+++ GuessingGame.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+void welcome()
+{
+    printf("Welcome to Guessing Game v1.0");
+}
+
+int main()
+{
+    srand(time(0));
+    int guessed_number = (rand() % 100) + 1;
+    int number;
+    
+    welcome();
+
+    bool play = true;
+    while(play)
+    {
+        printf("Guess the number!: ");
+        scanf("%d", &number);
+        if(number != 0)
+        {
+            
+            if(number == guessed_number)
+            {
+                printf("You are right! It is %d", guessed_number);
+                play = false;
+            }
+            else if(number < guessed_number)
+            {
+                printf("Less then guessed number. Try again!");
+            }
+            else if(number > guessed_number)
+            {
+                printf("More than guessed number. Try again!");
+            }
+        }
+        else
+        {
+            printf("Invalid input...");
+        }
+
+    }
+   
+    return 0;
+}
+
---
+++ RandomNumbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+    // pseudo random numbers = A set of values or elements that are statistically random
+    // (Don't use these for any sort of cryptographic security)
+
+
+    srand(time(0));
+
+    // generates random number between 1 and 6
+    int number1 = (rand() % 6) + 1;             // rand() function - random number between 0 and 32 767
+
+    printf("%d", number1);
+
+
+    return 0;
+}
